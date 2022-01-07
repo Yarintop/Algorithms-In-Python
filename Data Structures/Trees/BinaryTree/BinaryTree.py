8af54795035921f10d403cf66959b9d30bd3cded
Yarin <yarintop2@gmail.com>
2022-01-08 01:15:02 +0200

    Added search to binary tree

--- Data Structures/Trees/BinaryTree/BinaryTree.py
+++ Data Structures/Trees/BinaryTree/BinaryTree.py
@@ -23,8 +23,68 @@ class BinaryTree:
             else:
                 r.right = node
                 
-    def remove(self, node):
+    def remove(self, data):
+        pass
         
+    def search(self, data, node=None, order=-1):
+        if node == None:
+            if self.isEmpty():
+                return -1
+            node = self.root
         
-    def searchPreOrder(self, node):
+        def left():
+            if node.left:
+                l = self.searchPreOrder(data, node.left)
+                if l != -1:
+                    return l
+            return -1
+                
+        def middle():
+            if node.value == data:
+                return node
+            return -1
+            
+        def right():
+            if node.right:
+                r = self.searchPreOrder(data, node.right)
+                if r != -1:
+                    return r
+            return -1
         
+        def functionOrder(first, second, third):
+            f = first()
+            if f != -1:
+                return f
+            s = second()
+            if s != -1:
+                return s
+            t = third()
+            if t != -1:
+                return t
+            return -1
+                
+        if order == -1:
+            return functionOrder(middle, left, right)
+        elif order == 0:
+            return functionOrder(left, middle, right)
+        else:
+            return functionOrder(left, right, middle)
+        if node == None:
+            if self.isEmpty():
+                return -1
+            node = self.root
+        
+        if node.value == data:
+            return node
+        
+        if node.left:
+            l = self.searchPreOrder(data, node.left)
+            if l != -1:
+                return l
+            
+        if node.right:
+            r = self.searchPreOrder(data, node.right)
+            if r != -1:
+                return r
+            
+        return -1
