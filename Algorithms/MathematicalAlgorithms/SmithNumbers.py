eaf2ffe66a61dc9620eec7b88d0d4a81d72c7ea6
Yarin <yarintop2@gmail.com>
2022-01-15 07:07:52 +0200

    Added Smith Numbers Docstring

--- Algorithms/MathematicalAlgorithms/SmithNumbers.py
+++ Algorithms/MathematicalAlgorithms/SmithNumbers.py
@@ -3,6 +3,39 @@ from Algorithms.MathematicalAlgorithms.PrimeFactors import PrimeFactors
 class SmithNumbers:
     @staticmethod
     def smithNumbers(n):
+        """
+            Given a number n, the task is to find out whether this number is smith or not.
+            A Smith Number is a composite number whose sum of digits is equal to the sum of digits in its prime factorization.
+            
+            Examples:
+                Input  : n = 4
+                Output : Yes
+                Prime factorization = 2, 2  and 2 + 2 = 4
+                Therefore, 4 is a smith number
+
+                Input  : n = 6
+                Output : No
+                Prime factorization = 2, 3  and 2 + 3 is
+                not 6. Therefore, 6 is not a smith number
+
+                Input   : n = 666
+                Output  : Yes
+                Prime factorization = 2, 3, 3, 37 and
+                2 + 3 + 3 + (3 + 7) = 6 + 6 + 6 = 18
+                Therefore, 666 is a smith number
+
+                Input   : n = 13
+                Output  : No
+                Prime factorization = 13 and 13 = 13,
+                But 13 is not a smith number as it is not
+                a composite number
+
+        Args:
+            n (int): The number we're checking.
+
+        Returns:
+            Bool: True if n is a smith number, else False.
+        """
         primeFactors = PrimeFactors.primeFactors(n)
         
         if not primeFactors or primeFactors[0] == n:
